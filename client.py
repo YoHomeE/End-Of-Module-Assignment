bc9ddd28ae49175d3c614ca6e9b930ce379179cb
YoHomeE <136176988+YoHomeE@users.noreply.github.com>
2023-07-15 19:03:31 +0200

    Updateclient.py
    New Client, It still connects to the current server, it also creates a dictionary based on inputs. Currently the dictionary is for our group or other groups, When you enter a value for num_members it represents the number of group members you want a dictionary created for, the after that it will ask for inputs based on fixed criteria, name, surname and project role. The code then creates a list of containing multiple dictionaries for each group member.
    This list is subsequently written to a binary and JSON file based on your needs.
    I have the old code copy saved for control purposes

--- client.py
+++ client.py
@@ -1,33 +1,83 @@
 import socket
+import json
+import pickle
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "!DISCONNECT"
+"""Creates a class of Client, containing all the function thats clients should perform"""
+class Client:
+    """initialises the client with the necessary information to perform function"""
+    def __init__(self):
+        self.header = 64
+        self.port = 5050
+        self.server = socket.gethostbyname(socket.gethostname())
+        self.dict = [{}] 
+        self.client_socket = None
+        self.ADDR = (self.server, self.port)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# create the client
-client.connect(ADDR)
-# connect client to the server
+    """Connects the client to the server"""
+    def connect_to_server(self):
+        print(f'Connecting to server on {self.server} on Port {self.port} ') #f strings for dynamic updates
 
-
-def send(msg):
-    """
-    send message to the server
-    :param msg: message to be sent
-    :return: None
-    """
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b" " * (HEADER - len(send_length))
-    # make up to 64byte message length
-    # standardize the first message sent to server
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+        try:
+            self.client.connect(self.ADDR) #attempts connection
+            print("You have succesfully connected.")
+            return True
+        except ConnectionRefusedError: #code for failing to connect
+            print(f"Connection failed, please make sure there is a server \
+                  running on IP {self.server}: Port {self.port} ")
+            return False
 
 
-send("Hello World!")
+    #def disconnect(self): I have commented out this code as it is not doing anything yet, have not completed it
+        #if self.client is not None:
+            #disconnect = input("Would you like to disconnect : ")
+            #if disconnect == "Yes":
+                #self.client.close()
+
+    """Creates the dictionary - Going to work on making the keys dynamic(Inputs)"""
+    def createdict(self, num_members):
+        group_members = []
+
+        for member in range(num_members):
+            details = {
+                'Name': input("Please enter Student Name: "),
+                'Surname': input("Please enter Student Surname: "),
+                'Project Role': input("Please enter Project Role: ")
+            }
+            group_members.append(details)
+        return group_members
+
+    def savefile(self, group_members, selection): #Saves the file into JSON and Binary so far
+        if selection == "JSON":
+            with open("group-members.txt", 'w') as jsonfile:
+                jsonfile.write(json.dumps(group_members))
+                print("Successfully saved dictionary as JSON")
+            with open("group-members.txt", 'r') as jsonfile:
+                contents = jsonfile.read()
+                print(contents)
+
+        elif selection == "Binary":
+            with open("group-members.pkl", 'wb') as binfile:
+                pickle.dump(group_members, binfile)
+                print('Dictionary successfully saved as Binary File')
+            with open("group-members.pkl", 'rb') as binfile:
+                contents = binfile.read()
+                print(contents)
+        else: #Not sure if this is necessary now that it is in the __name__= __main__ but supposed to be text for invalid entry
+            print("Invalid selection. Please choose either 'JSON' or 'Binary'.")
+
+"""Engine behind the code, performs all the functions included in the class"""
+if __name__ == "__main__":
+    client = Client()
+    if client.connect_to_server():
+        num_members = int(input("Please enter the number of group members: "))
+        group_members = client.createdict(num_members)
+        print(group_members)
+
+        while True:
+            selection = input("Please select pickle format: JSON, Binary or XML: ")
+            if selection in ["JSON", "Binary"]:
+                client.savefile(group_members, selection)
+                break
+            else:
+                print("Invalid selection. Please choose either 'JSON' or 'Binary'.")
