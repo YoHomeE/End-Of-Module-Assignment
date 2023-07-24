19a1404c1fb3426eaf58386b29789afa9e4caba2
YoHomeE <136176988+YoHomeE@users.noreply.github.com>
2023-07-24 22:58:19 +0200

    Uploaded final version
    Uploaded the final version of client.py, this included:
    The addition of the help menu
    Each user input having exception handling
    An Integer handling exception function
    Docstrings, module docstrings and comments
    
    Final Version of Server includes:
    All the adequate docstrings
    A create file directory if not existing
    New save functions from Thomas
    Decryption calls utilising encryption module
    
    An Encryption.py Containing
    Encryption and decryption methods to help support server and client

--- client.py
+++ client.py
@@ -1,82 +1,339 @@
+"""
+Client Module
+-------------
+
+This module implements a client application that 
+allows users to create dictionaries with user-input keys and values.
+The client can choose to encrypt the dictionary 
+values using a simple substitution cipher.
+The client can save the created dictionary in JSON, Binary, or XML format.
+Additionally, the client can send the saved file to a connected server.
+
+Requirements:
+    - Python 3.6+
+    - socket
+    - json
+    - pickle
+    - string
+    - os
+    - dicttoxml
+    - encryption (custom module for data encryption)
+
+Functions:
+    intexception(prompt: str) -> int:
+        Function to handle integer input with exception handling.
+
+Classes:
+    Client:
+        The main client class that handles dictionary creation, 
+        saving files, and sending files to the server.
+
+Usage:
+    - Run the python3 client.py when the server is running
+    - The client will display a welcome message and 
+      provide instructions on how to use the program.
+    - Follow the prompts to create dictionaries, 
+      save them in the desired format, and optionally encrypt the values.
+    - If encryption is chosen, the client will use 
+      the "encryption" module to encrypt the dictionary values.
+    - The client can send the saved file to the connected server for further processing.
+
+Example:
+    $ python3 client.py
+
+Note:
+    The "encryption" module must be present in the 
+    same directory or included in the Python path for handling encrypted data.
+
+"""
 import socket
 import json
 import pickle
+import string
+import os
+import dicttoxml
 
-"""Creates a class of Client, containing all the function thats clients should perform"""
+import encryption
+
+BUFFER_SIZE = 4096
+FORMAT = "utf-8"
+ENCRYPT_STATUS = False
+
+def intexception(prompt):
+    """
+    Function to handle integer input with exception handling.
+
+    Args:
+        prompt (str): The prompt message for user input.
+
+    Returns:
+        int: The valid integer entered by the user.
+    """
+    while True:
+        try:
+            value = int(input(prompt))
+            if value < 0:
+                print("Invalid input, please enter a non-negative number")
+            else:
+                return value
+        except ValueError:
+            print("Invalid input, please enter an integer.")
+
+
 class Client:
-    """initialises the client with the necessary information to perform function"""
+    """
+    Represents a client-side application to create dictionaries, optionally encrypt their
+    keys and values, save them in JSON, Binary, or XML format, and send the file to a server.
+
+    Attributes:
+        header (int): The header size for messages sent to the server.
+        port (int): The port number for the server communication.
+        server (str): The server address (IP address or hostname).
+        dict (list): A list containing dictionaries created by the user.
+        client_socket (file object): The socket file object for communication with the server.
+        addr (tuple): A tuple containing the server address and port.
+        client (socket object): The client-side socket object.
+    """
     def __init__(self):
+        """
+        Initialize the Client class.
+        """
         self.header = 64
         self.port = 5050
         self.server = socket.gethostbyname(socket.gethostname())
+        self.dict = [{}]
         self.client_socket = None
-        self.ADDR = (self.server, self.port)
+        self.addr = (self.server, self.port)
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    """Connects the client to the server"""
-    def connect_to_server(self):
-        print(f'Connecting to server on {self.server} on Port {self.port} ') #f strings for dynamic updates
 
-        try:
-            self.client.connect(self.ADDR) #attempts connection
-            print("You have succesfully connected.")
-            return True
-        except ConnectionRefusedError: #code for failing to connect
-            print(f"Connection failed, please make sure there is a server \
-                  running on IP {self.server}: Port {self.port} ")
-            return False
+    def help_function(self):
+        """
+        Display information about how to use the program.
+        """
+        print("Welcome to the Client program.")
+        print("This program allows you to create dictionaries with user-input keys and values.")
+        print("You can choose to encrypt the dictionary keys \
+            and values using a simple substitution cipher.")
+        print("After creating the dictionary, you can save it in JSON, Binary, or XML format.")
+        print("Finally, you can choose to send the saved file to the connected server.")
+        print("To use the program, follow these steps:")
+        print("1. Enter the number of dictionaries you want to create.")
+        print("2. Enter the number of keys to include in each dictionary.")
+        print("3. Provide the keys and values for each dictionary.")
+        print("4. Optionally, choose to encrypt the values.")
+        print("5. Select the format (JSON, Binary, or XML) to save the data.")
+        print("6. If you choose to send the file to the server, it will be transferred.")
+        print("7. To exit the program, use 'ctrl + c' at any time.")
+        print("Enjoy using the Client program!")
+        print("To proceed with the program, simply follow the prompts below.")
 
-
-    #def disconnect(self): I have commented out this code as it is not doing anything yet, have not completed it
-        #if self.client is not None:
-            #disconnect = input("Would you like to disconnect : ")
-            #if disconnect == "Yes":
-                #self.client.close()
-
-    """Creates the dictionary - Going to work on making the keys dynamic(Inputs)"""
-    def createdict(self, num_members):
-        group_members = []
-
-        for member in range(num_members):
-            details = {
-                'Name': input("Please enter Student Name: "),
-                'Surname': input("Please enter Student Surname: "),
-                'Project Role': input("Please enter Project Role: ")
-            }
-            group_members.append(details)
-        return group_members
-
-    def savefile(self, group_members, selection): #Saves the file into JSON and Binary so far
-        if selection == "JSON":
-            with open("group-members.txt", 'w') as jsonfile:
-                jsonfile.write(json.dumps(group_members))
-                print("Successfully saved dictionary as JSON")
-            with open("group-members.txt", 'r') as jsonfile:
-                contents = jsonfile.read()
-                print(contents)
-
-        elif selection == "Binary":
-            with open("group-members.pkl", 'wb') as binfile:
-                pickle.dump(group_members, binfile)
-                print('Dictionary successfully saved as Binary File')
-            with open("group-members.pkl", 'rb') as binfile:
-                contents = binfile.read()
-                print(contents)
-        else: #Not sure if this is necessary now that it is in the __name__= __main__ but supposed to be text for invalid entry
-            print("Invalid selection. Please choose either 'JSON' or 'Binary'.")
-
-"""Engine behind the code, performs all the functions included in the class"""
-if __name__ == "__main__":
-    client = Client()
-    if client.connect_to_server():
-        num_members = int(input("Please enter the number of group members: "))
-        group_members = client.createdict(num_members)
-        print(group_members)
-
+    def run(self):
+        """
+        Start the client application and display the help message if requested.
+        """
         while True:
-            selection = input("Please select pickle format: JSON, Binary or XML: ")
-            if selection in ["JSON", "Binary"]:
-                client.savefile(group_members, selection)
+            display_help = input("Would you like help on how to use the program? (Yes/No): ")
+            if display_help == "Yes":
+                self.help_function()
+                return
+            elif display_help == "No":
                 break
             else:
-                print("Invalid selection. Please choose either 'JSON' or 'Binary'.")
+                print("Invalid Selection, Please choose either Yes or No")
+
+
+    def connect_to_server(self):
+        """
+        Connect to the server using the specified address and port.
+
+        Returns:
+            bool: True if connection is successful, False otherwise.
+        """
+        print(f"Connecting to server on {self.server} on Port {self.port}")
+
+        try:
+            self.client.connect(self.addr)
+            self.client_socket = self.client.makefile("wb")
+            print("You have successfully connected.")
+            return True
+        except ConnectionRefusedError:
+            print(
+                f"Connection failed. Please make sure there \
+                    is a server running on IP {self.server}: Port {self.port}"
+            )
+            return False
+
+    def send_data(self, msg: str):
+        """
+        encode str to byte
+        send byte data to the server
+        send a header msg of byte length
+        """
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b" " * (BUFFER_SIZE - len(send_length))
+        # make up to header message length
+        self.client.sendall(send_length)
+        self.client.sendall(message)
+
+    def receive_data(self):
+        """
+        receive byte data sent from server
+        return: decoded msg
+        """
+        msg_length = self.client.recv(BUFFER_SIZE).decode(FORMAT)
+        # receive the data length in bytes
+        if msg_length:  # avoid error with empty msg upon connection
+            msg_length = int(msg_length)
+            msg = self.client.recv(msg_length).decode(FORMAT)
+            # receive the actual message for the exact byte length
+            print(f"[SERVER] {msg}")  # print to console for debugging
+            return msg
+
+    def createdict(self, num_members, num_keys):
+        """
+        Create a dictionary with user-input keys and values.
+
+        Args:
+            num_members (int): The number of dictionaries to create.
+            num_keys (int): The number of keys to include in each dictionary.
+
+        Returns:
+            list: A list containing the created dictionaries.
+        """
+        listofdicts = [] #Named Variable list of Dicts from Group Members
+
+        for member in range(num_members):
+            details = {}
+            for k in range(num_keys):
+                key = input("Please Enter your Key: ")
+                value = input("Please enter your value: ")
+                details[key] = value
+            listofdicts.append(details)
+        return listofdicts
+
+    def savefile(self, listofdicts, selection):
+        """
+        Save the listofdicts dictionary to a file based on the selection format.
+
+        Args:
+            listofdicts (list): The list of dictionaries to save.
+            selection (str): The format to save the data as (JSON, Binary, or XML).
+
+        Returns:
+            str: The name of the saved file.
+        """
+        if selection == "JSON":
+            filename = input("Please name your file (Do not add the .format) : ")
+            filename += ".json"
+            with open(filename, "w") as jsonfile:
+                jsonfile.write(json.dumps(listofdicts))
+                print("Successfully saved dictionary in JSON with name", filename)
+            with open(filename, "r") as jsonfile:
+                contents = jsonfile.read()
+                print(contents)
+                return filename
+
+        elif selection == "Binary":
+            filename = input("Please name your file (The code with add the .format) : ")
+            filename += ".pkl"
+            with open(filename, "wb") as binfile:
+                pickle.dump(listofdicts, binfile)
+                print("Dictionary successfully saved in Binary with name", filename)
+            with open(filename, "rb") as binfile:
+                contents = binfile.read()
+                print(contents)
+                return filename
+
+        elif selection == "XML":
+            filename = input("Please name your file (The code with add the .format) : ")
+            filename += ".xml"
+            xml_string = dicttoxml.dicttoxml(listofdicts, attr_type=False)
+            with open(filename, "w") as xmlfile:
+                xmlfile.write(xml_string.decode())
+                print("Successfully saved dictionary in XML with name", filename)
+            with open(filename, "r") as xmlfile:
+                contents = xmlfile.read()
+                print(contents)
+                return filename
+
+        else:
+            print("Invalid selection. Please choose either 'JSON', 'Binary', or 'XML'.")
+
+    def send_file(self, filename, encryption_status):
+        """
+        Send a file to the connected server.
+
+        Args:
+            filename (str): The name of the file to be sent.
+
+        Raises:
+            FileNotFoundError: If the specified file does not exist.
+
+        Returns:
+            None
+        """
+        if not os.path.exists(filename):
+            raise FileNotFoundError(f"The file '{filename}' does not exist.")
+        # Encode the metadata and send it as bytes by calling send_data function
+        metadata_filename = filename
+        self.send_data(metadata_filename)  # send filename
+        if encryption_status == True:
+            metadata_encrypt_status = "encrypted"
+        else:
+            metadata_encrypt_status = "not encrypted"
+        self.send_data(metadata_encrypt_status)
+        with open(filename, "rb") as file:
+            while True:
+                bytes_read = file.read(BUFFER_SIZE)  # read with BUFFER SIZE
+                if not bytes_read:
+                    # Transfer is done. Nothing left to be sent.
+                    break
+                # Send what has been read so far.
+                self.client.sendall(bytes_read)
+
+        print(f"{filename} was sent to the server.")
+        self.receive_data()  # receive confirmation from server for metadata
+        self.receive_data()  # receive confirmation from server for file saving
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.run()
+    if client.connect_to_server():
+        num_members = intexception("Please enter the number of Dictionaries: ")
+        # Updated to use intexception function to prevent and loop back upon invalid entry
+        num_keys = intexception("Please enter the number of Keys: ")
+        listofdicts = client.createdict(num_members, num_keys)
+        print("Is this the dictionary you wanted?")
+        print(listofdicts)
+
+        encrypt_values = input("Encrypt values? (Yes/No): ")
+        while encrypt_values not in ["Yes", "No"]:
+            print("Invalid input. Please enter either 'Yes' or 'No'.")
+            encrypt_values = input("Encrypt values? (Yes/No): ")
+
+        if encrypt_values == "Yes":
+            listofdicts = encryption.encrypt_list_of_dicts(listofdicts)
+            ENCRYPT_STATUS = True
+
+        else:
+            print("Skipping encryption. Proceeding with the original dictionary.")
+
+        while True:
+            selection = input("Please select pickle format: JSON, Binary, or XML: ")
+            if selection in ["JSON", "Binary", "XML"]:
+                filename = client.savefile(listofdicts, selection)
+                break
+            else:
+                print(
+                    "Invalid selection. Please choose either 'JSON', 'Binary', or 'XML'."
+                )
+
+        send_to_server = input("Would you like to send the file to the server ? (Yes/No) : ")
+        if send_to_server == "Yes":
+            client.send_file(filename, ENCRYPT_STATUS)
+        else:
+            print("Send file canceled.")
---
+++ encryption.py
@@ -0,0 +1,102 @@
+"""
+Substitution Cipher Module
+
+This module provides functions for encrypting 
+and decrypting text using a simple substitution cipher.
+"""
+
+import string
+
+CHAR_SET = string.printable[:-5]
+SUBSTITUTION_CHARS = CHAR_SET[-3:] + CHAR_SET[:-3]
+
+ENCRYPTION_DICT = {}
+DECRYPTION_DICT = {}
+
+for i, k in enumerate(CHAR_SET):
+    v = SUBSTITUTION_CHARS[i]
+    ENCRYPTION_DICT[k] = v
+    DECRYPTION_DICT[v] = k
+
+
+def encrypt_msg(plaintext):
+    """
+    Encrypt the given plaintext using a simple substitution cipher.
+
+    Args:
+        plaintext (str): The plaintext to be encrypted.
+
+    Returns:
+        str: The encrypted ciphertext.
+    """
+    ciphertext = []
+    for k in plaintext:
+        v = ENCRYPTION_DICT.get(k, k)
+        ciphertext.append(v)
+    return "".join(ciphertext)
+
+
+def decrypt_msg(ciphertext):
+    """
+    Decrypt the given ciphertext using the reverse substitution cipher.
+
+    Args:
+        ciphertext (str): The ciphertext to be decrypted.
+
+    Returns:
+        str: The decrypted plaintext.
+    """
+    plaintext = []
+    for k in ciphertext:
+        v = DECRYPTION_DICT.get(k, k)
+        plaintext.append(v)
+    return "".join(plaintext)
+
+
+def encrypt_list_of_dicts(plaintext):
+    """
+    Encrypt the given plaintext using a simple substitution cipher.
+
+    Args:
+        plaintext (str): The plaintext to be encrypted.
+
+    Returns:
+        str: The encrypted ciphertext.
+    """
+
+    encrypted_output = []
+    for member in plaintext:
+        encrypted_text = {}
+        for key, value in member.items():
+            encrypted_key = encrypt_msg(key)
+            encrypted_value = encrypt_msg(value)
+            encrypted_text[encrypted_key] = encrypted_value
+        encrypted_output.append(encrypted_text)
+    print("Encrypted dictionary:")
+    print(encrypted_output)
+    return encrypted_output  #moved the return call so it encrypts multiple dictionaires
+
+
+def decrypt_list_of_dicts(ciphertext):
+    """
+    Decrypt the given ciphertext using the reverse substitution cipher.
+
+    Args:
+        ciphertext (str): The ciphertext to be decrypted.
+
+    Returns:
+        str: The decrypted plaintext.
+    """
+
+    decrypted_output = []
+    for member in ciphertext:
+        decrypted_text = {}
+        for key, value in member.items():
+            decrypted_key = decrypt_msg(key)
+            decrypted_value = decrypt_msg(value)
+            decrypted_text[decrypted_key] = decrypted_value
+        decrypted_output.append(decrypted_text)
+    print("Decrypted dictionary:")
+    #Changed the indenting so it Moves decrypts multilpe dictionairies
+    print(decrypted_output)
+    return decrypted_output
