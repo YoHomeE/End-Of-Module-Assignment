6d1701f467632163cefe3182b9409d0a276b7dff
YoHomeE <136176988+YoHomeE@users.noreply.github.com>
2023-07-27 23:43:13 +0200

    Add files via upload

---
+++ Tests/testdata.py
@@ -0,0 +1,11 @@
+
+dict1 = [{"name" : "nicholas"}]
+
+dict2 = [{"hobby" : "Surfing", "number" : "10"}]
+
+dict3 = [{"group" : "b", "project" : "server", "Due-Date" : "31"}]
+
+dict4 = [{"this" : "one", "will": "be", "extra" : "lonng", "forr": "testting", "sake" : "ahaha", "longwordscuzwhynot" : "letsseeifcanhandle"}]
+
+emptydict = [{}]
+
